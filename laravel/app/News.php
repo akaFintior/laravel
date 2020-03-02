514311d5e793f72ea63f085928ec2fc406b9ce63
akaFintior <akovaliov.91@gmail.com>
2020-03-02 21:29:01 +0000

    lesson-7 laravel validation added to addNews

--- laravel/app/News.php
+++ laravel/app/News.php
@@ -12,4 +12,23 @@ class News extends Model
     public  function category() {
         return $this->belongsTo(Category::class, 'category_id')->first();
     }
+
+    public static function rules() {
+        $tableNameCategory = (new Category())->getTable();
+        return [
+            'title' => 'required|min:10|max:20',
+            'inform' => 'required',
+            'category_id' => "required|exists:{$tableNameCategory},id",
+            'image' => 'mimes:jpeg,bmp,png|max:1000'
+
+        ];
+    }
+    public static function attributeNames() {
+        return [
+            'title' => 'Заголовок новости',
+            'inform' => 'Текст новости',
+            'category_id' => "Категория новости",
+            'image' => "Изображение"
+        ];
+    }
 }
